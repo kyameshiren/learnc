83617b67348d8dba0e03e4504ebcb70000faff1c
Kyame Shiren <mrbossstyle@gmail.com>
2025-05-08 01:53:49 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # learnc
+- 05/08/2025 : Hello world, basic math and strings
---
+++ t.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* Generates Fahrenheit to Celsius table */
+
+int main()
+{
+        int fahr;
+        float cels;
+
+        printf("  F : C\n");
+        for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+        {
+                cels = (5.0/9.0) * (fahr-32);
+                printf("%3d : %.2f\n", fahr, cels);
+        }
+}
